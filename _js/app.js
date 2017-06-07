de7091786f9efc74ff4e319c856a7ae18972bd96
QnX <korenev@gmail.com>
2017-06-07 12:40:53 +0400

    filter & sort in the projects table

--- _js/app.js
+++ _js/app.js
@@ -2,10 +2,33 @@
 
 var projectApp = angular.module('projectApp',[]);
 
+projectApp.controller('AllProjectsCtrl',['$scope','$http',function($scope,$http){
+	$http.get('_data/projects.data.json')
+	.then(function(response){
+		$scope.projects = response.data;
+		console.log("Data:", response.data);
+	});
+	$scope.projectView = function(id){
+		console.log("кликнули на строку проекта c ID=",id);
+	};
+		
+	$scope.sortField = undefined;
+	$scope.reverse = false;
+
+	$scope.sort = function(fieldName){
+		if ($scope.sortField === fieldName) {
+			$scope.reverse = !$scope.reverse;
+		} else {
+			$scope.sortField = fieldName;
+			$scope.reverse = false;
+		}
+	};
+}]);
+
 projectApp.controller('projectCtrl',['$scope', '$http', function($scope, $http){
 	$http.get('_data/project.data.json')
 	.then(function(response){
 		$scope.project = response.data;
-		console.log("Data:", response.data);
+		// console.log("Data:", response.data);
 	});
 }]);
